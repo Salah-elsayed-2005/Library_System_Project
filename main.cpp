769642ce3512f8b8f4b60fd5e4fce98c04d218d0
Soliman Elhassanein <133492248+Soliman-Elhassanein@users.noreply.github.com>
2024-03-08 17:34:52 +0200

    str commit
    In this commit @Soliman-Elhassanein  added the code of the str class.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include "str.h"
 using namespace std;
-int main() {
-     return 0;
+using namespace OurString;
+
+
+int main()
+{
+
+	/* Testing str class, and all its functions
+		str x = "hiiiiii";
+		str y = x;
+		cout << x << endl;
+		cout << y << endl;
+
+		cout << (x == y) << endl;
+
+		y = "hiiiiix";
+		cout << (x == y) << endl;
+
+
+		y = "hiiiy";
+		cout << (x == y) << endl;
+
+		cout << y.len() << endl;
+
+		x = y;
+		cout << x << endl;
+	*/
+
 }
+
