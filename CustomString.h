718532749446568a5a3b8dcda06b804b41fb3eec
Salah-elsayed-2005 <118000804+Salah-elsayed-2005@users.noreply.github.com>
2024-03-11 15:52:03 +0200

    Delete CustomString.h

--- CustomString.h
+++
@@ -1,9 +0,0 @@
-//
-// Soliman Part
-// len-concat-delete-assignment-comparison-removing spaces-etc
-//
-
-#ifndef FIRST_PATCH___GITHUB_CUSTOMSTRING_H
-#define FIRST_PATCH___GITHUB_CUSTOMSTRING_H
-
-#endif //FIRST_PATCH___GITHUB_CUSTOMSTRING_H
